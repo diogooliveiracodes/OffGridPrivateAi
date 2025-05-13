ff4d17fb5fe2968c28860bc6a555b792d8849a27
Diogo Oliveira <diogo.o@icloud.com>
2025-05-13 09:20:03 -0300

    refactor: rename HomeView to ChatView and update routing delete: remove unused HelloWorld component and logo

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ChatView from '../views/ChatView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: ChatView
   },
   {
     path: '/about',
---
+++ app/src/views/ChatView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+    
+  },
+})
+export default class HomeView extends Vue {
+
+}
+</script>
